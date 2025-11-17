337c5404b20be415e1f7645d81aec08c0692a74a
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-11-17 14:00:12 +0500

    some bugs fix

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,9 +9,6 @@ const { Search } = Input;
 
 const Navbar = () => {
     const { pathname } = useLocation();
-    console.log(location.pathname);
-
-    // const url = window.location.href
 
     const [value, setValue] = useState<string>("")
     const searchValue = useDebounceHook(value)
@@ -31,9 +28,7 @@ const Navbar = () => {
             {
                 !hideSearch &&
                 <Search value={value} onChange={(e) => setValue(e.target.value)} placeholder="Search" enterButton style={{ width: 400 }} />
-
             }
-            <div>test</div>
         </div>
     )
 }
